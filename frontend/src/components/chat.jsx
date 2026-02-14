1dcbf41a9df476b9e31210c3b7d83cf8bc887232
Evan Nicholas <evancninja@gmail.com>
2026-02-13 20:52:05 -0500

    Build Changes

---
+++ frontend/src/components/chat.jsx
@@ -0,0 +1,3 @@
+export default function Chat({message}) { 
+    return(<p> {message} </p>);
+}
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
    // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Rem/',  // Your repo name
+  base: '/Rem/',
   build: {
-    outDir: '../docs'  // Goes up one level and creates 'docs' folder
+    outDir: '../docs'
   }
 })
